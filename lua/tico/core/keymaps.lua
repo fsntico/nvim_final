6542182ef3ec837d91fc346be3ffd6c8e23bf05c
fsntico <fsntico@gmail.com>
2024-04-26 09:27:52 -0300

    26/04/2024

--- lua/tico/core/keymaps.lua
+++ lua/tico/core/keymaps.lua
@@ -17,8 +17,8 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- keymap.set("n", "x", '"_x')
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })      -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })      -- decrement
 keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true }) -- decrement
 
 
